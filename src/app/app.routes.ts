6a0d6c60c6f900e88fbb0e3895fac4ea7378dd06
vinay-easyres <vinay.kumar@easyres.in>
2024-10-09 11:41:20 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,47 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PropertyComponent } from './pages/property/property.component';
+import { SiteComponent } from './pages/site/site.component';
+import { BookingComponent } from './pages/booking/booking.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: "",
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: 'property-type',
+                component: PropertyComponent
+            },
+            {
+                path: 'site',
+                component: SiteComponent
+            },
+            {
+                path: 'booking',
+                component: BookingComponent
+            },
+            {
+                path: '**',
+                redirectTo: 'dashboard'
+            }
+        ]
+
+    }
+
+];
